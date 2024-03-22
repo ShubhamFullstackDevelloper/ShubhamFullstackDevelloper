6a30be84f94d9c5d68160b995d7f516fcd6effe6
ShubhamFullstackDevelloper <158504898+ShubhamFullstackDevelloper@users.noreply.github.com>
2024-03-21 21:56:04 -0700

    Delete otp.js

--- otp.js
+++
@@ -1,9 +0,0 @@
-function genarateOpt( length){
-    let otp='';
-    for(let i=0; i<length;  i++){
-        opt+=Math.floor(Math.random()*10).toString();
-    }
-    return otp;
-
-}
-console. genarateOptlog((4));
